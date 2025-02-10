ce9f12db29da9103cf0332a5dd71079812389e5d
Daniel Kawasaka <dkawasaka@gmail.com>
2025-02-10 06:39:05 -0300

    Add funcionarios table definition with UUID and user details

--- model/db_wizard.py
+++ model/db_wizard.py
@@ -10,4 +10,14 @@ User = db.define_table('auth_user',
         migrate=DB_MIGRATE
         )
 
+Funcionar = db.define_table('funcionarios',
+        Field('uuid',       length=64,  default=lambda:str(uuid.uuid4()),   writable=False,	readable=False),
+        Field('nome', type='string', required=True, notnull=True),
+        Field('cargo', type='string', length=50),
+        Field('departamento', type='string', length=50),
+        Field('email', type='string', length=180),
+        Field('is_active', type='boolean', default=True, required=True, notnull=True),
+        migrate=DB_MIGRATE
+)
+
 db.commit()
